7ca405216904b8a2b411f6fb4219865c1eda16c7
Cody A Fagley <Cody.Fagley@gmail.com>
2017-05-15 11:55:58 -0600

    Restructuring

---
+++ LendingLibrary/ProgWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LendingLibrary
+{
+    public partial class ProgWindow : Form
+    {
+        public ProgWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProgWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
