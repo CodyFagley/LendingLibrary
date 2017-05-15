3107d1b3b0d1e645adedaf8bccf60e6c45bd4bbf
Cody A Fagley <Cody.Fagley@gmail.com>
2017-05-15 00:05:04 -0600

    Project Init

---
+++ LendingLibrary/Items.table.sql
@@ -0,0 +1,28 @@
+﻿/* 
+ *	Items.table.sql
+ *	Cody Fagley
+ *	Authored on May 14, 2017
+ */
+
+/*
+ *	Table containing lent item information
+ */
+CREATE TABLE [dbo].[Items]
+(
+	--  Primary Key
+	ITEM_ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	
+	--  Name of item being lent
+	ITEM_NAME varchar NOT NULL,
+	--  Person whom the item is lent to
+	ITEM_LENDEE varchar NOT NULL,
+	
+	--  OPTIONAL - Date that item is lent: YYYY-MM-DD
+	ITEM_DATE_LENT date NULL,
+	--  OPTIONAL - Date that item is returned: YY-MM-DD
+	ITEM_DATE_RETURNED date NULL,
+	
+	--  OPTIONAL - Description of the item
+	ITEM_DESCRIPTION varchar NULL,
+
+)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# LendingLibrary
+# Cody && Lisa's Lending Library
+This serves as a dual-purpose project.
+
+*  First, it is my code example for a C# w/ Microsoft SQL 2008 server
+*  Second, it tracks items that we lend to friends and family
+
